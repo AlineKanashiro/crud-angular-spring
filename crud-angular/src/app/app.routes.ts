61af891057142f171f1d46275db2155bbec84003
Aline Kanashiro <aline.k.kanashiro@gmail.com>
2025-05-21 21:52:28 -0300

    Criando o módulo de cursos e usando roteamento com Lazy Loading

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { CoursesModule } from './courses/courses.module';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
